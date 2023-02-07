e1ec1b8934e2a6f2518d1de34df6816a3b7228e1
serenity smith <serenitysmith24@gmail.com>
2023-02-07 00:03:38 -0500

    loop olympics

---
+++ app.js
@@ -0,0 +1,88 @@
+// var numArray= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]; 
+// for(var i =  0; i < numArray.length; i++){
+//     console.log(numArray[i]);
+
+// }
+// going backwards in my array
+// var numArray= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]; 
+// for( var i = numArray.length -1; i >= 0; i--){
+//     console.log(numArray[i]);
+// }
+// logs fruit to console
+// const fruit = ["banana", "orange", "apple", "kiwi"]
+// for(var i = 0; i < fruit.length; i++){
+//     console.log(fruit[i])
+// }
+
+// pusjed 0-9 into an empty array
+
+// function factor (num) {
+//     var myNum = []
+//     for (var i = 0; i <= num ; i++){
+//         myNum.push(i)
+//     }
+//     return myNum
+    
+// }
+// console.log(factor(9));
+
+
+
+// loop with number 1 - 100 even numbers only
+// for(var i = 0; i <=100; i++)
+// {
+//     if (i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+// const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+//logs every other fruit
+// for(var i = 0; i < fruit.length; i = i +2){
+// console.log(fruit[i])
+// }
+
+const peopleArray = [
+    {
+      name: "Harrison Ford",
+      occupation: "Actor"
+    },
+    {
+      name: "Justin Bieber",
+      occupation: "Singer"
+    },
+    {
+      name: "Vladimir Putin",
+      occupation: "Politician"
+    },
+    {
+      name: "Oprah",
+      occupation: "Entertainer"
+    }
+  ]
+  // run through the loop normally
+  //for(var i = 0; i < peopleArray; i++)
+  
+  // runs through every other name in array
+// for(var i =0; i < peopleArray.length; i = i +2){
+//   console.log (peopleArray [i].name)
+// }
+
+
+// logs every other occuaption 
+// for(var i =0; i < peopleArray.length; i = i +2){
+//   console.log (peopleArray [i].occupation)
+// }
+// logs ever other name and occupation
+// for(var i =0; i < peopleArray.length; i = i +2){
+//   console.log (peopleArray [i].name + peopleArray [i].occupation)
+// }
+
+    //logs names only
+  //console.log(peopleArray [i].name)
+  //logs names and occupations
+  //console.log (peopleArray [i].name + peopleArray [i].occupation )
+ 
+  // ["Harrison Ford", "Vladimir Putin"] // names
+  // ["Singer", "Entertainer"] // occupations
+  //+ users[i].age
+  
